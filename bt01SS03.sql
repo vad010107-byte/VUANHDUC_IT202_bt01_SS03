f228ecc20f7154943685ac698a9f483909b08ccf
vad010107-byte <vad010107@gmail.com>
2026-04-16 22:07:52 +0700

    Add files via upload

--- bt01SS03.sql
+++ bt01SS03.sql
@@ -1,4 +1,17 @@
-UPDATE Products
-SET Price = Price * 0.9
-WHERE CategoryName = 'Electronics';
--- giữ nguyên toàm bộ logic code toàn bộ đề bài chỉ thêm where để code hiểu là cần update ở đâu
+
+CREATE TABLE PRODUCTS (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    OriginalPrice DECIMAL(18, 2)
+);
+INSERT INTO PRODUCTS (ProductID, ProductName, Category, OriginalPrice)
+VALUES
+(1, 'iPhone 15', 'Electronics', 20000000),
+(2, 'Samsung Refrigerator', 'Electronics', 15000000),
+(3, 'Water Spinach', 'Food', 10000),
+(4, 'Filtered Fresh Milk 4', 'Food', 28000);
+UPDATE PRODUCTS
+SET OriginalPrice = OriginalPrice * 0.9
+WHERE Category = 'Electronics';
+
